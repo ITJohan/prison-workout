b74a7aa51459ee6cce96c129b3007e09364d75a6
Johan Lindkvist <johanlindkvist89@gmail.com>
2020-10-29 15:14:02 +0100

    Add working base version

--- public/app.js
+++ public/app.js
@@ -1,61 +1,133 @@
+// Firebase setup
 const auth = firebase.auth()
-
-const whenSignedIn = document.getElementById('whenSignedIn')
-const whenSignedOut = document.getElementById('whenSignedOut')
-const signInBtn = document.getElementById('signInBtn')
-const signOutBtn = document.getElementById('signOutBtn')
-const userDetails = document.getElementById('userDetails')
-
+const db = firebase.firestore()
 const provider = new firebase.auth.GoogleAuthProvider()
 
-signInBtn.onclick = () => auth.signInWithPopup(provider)
+// DOM sections
+const mainSection = document.getElementById('main-section')
 
-signOutBtn.onclick = () => auth.signOut()
-
-auth.onAuthStateChanged(user => {
-  if (user) {
-    whenSignedIn.hidden = false
-    whenSignedOut.hidden = true
-    userDetails.innerHTML = `<h3>Hello ${user.displayName}!</p>`
-  } else {
-    whenSignedIn.hidden = true
-    whenSignedOut.hidden = false
-    userDetails.innerHTML = ''
+// Exercises blueprints
+const workoutA = [
+  {
+    name: 'Pistol squats',
+    sets: [0, 0, 0],
+  },
+  {
+    name: 'Pull-ups',
+    sets: [0, 0, 0],
+  },
+  {
+    name: 'Chest dips',
+    sets: [0, 0, 0]
+  },
+  {
+    name: 'Leg raises',
+    sets: [0, 0, 0]
   }
-})
+]
 
-const db = firebase.firestore()
+const workoutB = [
+  {
+    name: 'Schrimp squats',
+    sets: [0, 0, 0],
+  },
+  {
+    name: 'Front lever row',
+    sets: [0, 0, 0],
+  },
+  {
+    name: 'Handstand push-ups',
+    sets: [0, 0, 0]
+  },
+  {
+    name: 'Dragon flag',
+    sets: [0, 0, 0]
+  }
+]
 
-const createThing = document.getElementById('createThing')
-const thingsList = document.getElementById('thingsList')
+const generateForm = (exercises) => {
+  return `
+    <form id="form">
+      <h3 id="set-1-header">Set 1</h3>
+      <h3 id="set-2-header">Set 2</h3>
+      <h3 id="set-3-header">Set 3</h3>
+      <h3 id="exercise-1-name">${exercises[0].name}</h3>
+      <input id="exercise-1-set-1-input" type="number" min="0" required placeholder=${exercises[0].sets[0]}>
+      <input id="exercise-1-set-2-input" type="number" min="0" required placeholder=${exercises[0].sets[1]}>
+      <input id="exercise-1-set-3-input" type="number" min="0" required placeholder=${exercises[0].sets[2]}>
+      <h3 id="exercise-2-name">${exercises[1].name}</h3>
+      <input id="exercise-2-set-1-input" type="number" min="0" required placeholder=${exercises[1].sets[0]}>
+      <input id="exercise-2-set-2-input" type="number" min="0" required placeholder=${exercises[1].sets[1]}>
+      <input id="exercise-2-set-3-input" type="number" min="0" required placeholder=${exercises[1].sets[2]}>
+      <h3 id="exercise-3-name">${exercises[2].name}</h3>
+      <input id="exercise-3-set-1-input" type="number" min="0" required placeholder=${exercises[2].sets[0]}>
+      <input id="exercise-3-set-2-input" type="number" min="0" required placeholder=${exercises[2].sets[1]}>
+      <input id="exercise-3-set-3-input" type="number" min="0" required placeholder=${exercises[2].sets[2]}>
+      <h3 id="exercise-4-name">${exercises[3].name}</h3>
+      <input id="exercise-4-set-1-input" type="number" min="0" required placeholder=${exercises[3].sets[0]}>
+      <input id="exercise-4-set-2-input" type="number" min="0" required placeholder=${exercises[3].sets[1]}>
+      <input id="exercise-4-set-3-input" type="number" min="0" required placeholder=${exercises[3].sets[2]}>
+      <button type="submit" id="submit-button">Finished!</button>
+    </form>
+    <br>
+    <button id="sign-out-btn">Sign out</button>
+  `
+}
 
-let thingsRef
-let unsubscribe
+const getLatestWorkout = async (user) => {
+  
+}
 
-auth.onAuthStateChanged(user => {
+auth.onAuthStateChanged(async user => {
   if (user) {
-    thingsRef = db.collection('workouts')
+    // Logged in
 
-    createThing.onclick = () => {
-      const { serverTimestamp } = firebase.firestore.FieldValue
+    const workoutsRef = db.collection('workouts')
+    const snapshot = await workoutsRef
+      .where('uid', '==', user.uid)
+      .get()
 
-      thingsRef.add({
-        uid: user.uid,
-        name: 'bert',
-        createdAt: serverTimestamp()
-      })
+    let workout
+
+    if (snapshot.empty) {
+      // No workouts so default to A with no reps
+      workout = [ ...workoutA ]
+    } else if (snapshot.size === 1) {
+      // One workout so default to B with no reps
+      workout = [ ...workoutB ]
+    } else {
+      // Two or more workouts so default to previous of same
+      const workouts = snapshot.docs.map(doc => doc.data()).sort((a, b) => b.date - a.date)
+      workout = workouts[1].exercises
     }
 
-    unsubscribe = thingsRef
-      .where('uid', '==', user.uid)
-      .onSnapshot(querySnapshot => {
-        const items = querySnapshot.docs.map(doc => {
-          return `<li>${doc.data().name}`
-        })
+    mainSection.innerHTML = generateForm(workout)
 
-        thingsList.innerHTML = items.join('')
+    const signOutBtn = document.getElementById('sign-out-btn')
+    const form = document.getElementById('form')
+
+    signOutBtn.addEventListener('click', () => auth.signOut())
+    form.addEventListener('submit', async e => {
+      e.preventDefault()
+
+      const exercises = [ ...workout ]
+      const data = Array.from(e.target.elements).map(e => Number(e.value))
+      exercises.forEach((exercise) => {
+        exercise.sets = data.splice(0, 3)
       })
+
+      workoutsRef.add({
+        uid: user.uid,
+        date: firebase.firestore.FieldValue.serverTimestamp(),
+        exercises
+      })
+    })
   } else {
-    unsubscribe && unsubscribe()
+    // Logged out
+    mainSection.innerHTML = `
+      <button id="sign-in-btn">Sign in with Google</button>
+    `
+    const signInBtn = document.getElementById('sign-in-btn')
+    signInBtn.addEventListener('click', () => auth.signInWithPopup(provider))
   }
 })
